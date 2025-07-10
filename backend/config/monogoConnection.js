0c583911433fd2ad3af860779a7f7abde902c23b
iskander brahim <iskander.skan05@yahoo.com>
2025-07-10 22:30:41 +0100

    first try

--- backend/config/monogoConnection.js
+++ backend/config/monogoConnection.js
@@ -3,10 +3,7 @@ import mongoose from 'mongoose';
 const mongoURI = "mongodb+srv://iskander:Topadmin2005@cluster0.wkuwwi9.mongodb.net/makinapack?retryWrites=true&w=majority&appName=Cluster0"
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(mongoURI);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.log('Error connecting to MongoDB:');
